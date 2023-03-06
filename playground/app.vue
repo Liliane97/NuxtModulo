31c6f31bb6da1cd1907aff4405590de9d17d7353
liliane97 <lilianebasilio1997@gmail.com>
2023-03-06 20:49:57 -0300

    criando modulo nuxt

---
+++ playground/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ProjetoModule/>
+  </div>
+</template>
+
+<script setup>
+import ProjetoModule from '../dist/runtime/components/projetoModule.vue';
+
+</script>
---
+++ src/module.ts
@@ -0,0 +1,34 @@
+import { defineNuxtModule, addPlugin, createResolver, addComponent } from '@nuxt/kit'
+import { resolve } from 'path/posix';
+import { name, version } from '../package.json';
+
+// Module options TypeScript inteface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name,
+    version,
+    configKey: 'projetoModule',
+    compatibility:{
+      nuxt:"^3.0.0",
+    }
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup (_moduleOptions, nuxt) {
+    const resolver = createResolver(import.meta.url)
+    const runtimeDir = resolver.resolve("./runtime")
+    nuxt.options.build.transpile.push(runtimeDir)
+
+    addPlugin(resolve(runtimeDir, 'plugin'))
+
+   const componentsDir = resolver.resolve(runtimeDir,"components");
+
+   addComponent({
+    name:"ProjetoModule",
+    filePath:resolve(componentsDir,"ProjetoModule.vue")
+   })
+
+  }
+})
